db0d2cd336c066cf979d7e69d42240cd46702a34
David Michael <1.david.michael@gmail.com>
2021-07-05 11:12:12 -0500

    Initial commit

---
+++ lib/index.ts
@@ -0,0 +1,14 @@
+import * as cdk from '@aws-cdk/core';
+
+export interface CloudwatchAlarmsToTeamsConstructProps {
+  // Define construct properties here
+}
+
+export class CloudwatchAlarmsToTeamsConstruct extends cdk.Construct {
+
+  constructor(scope: cdk.Construct, id: string, props: CloudwatchAlarmsToTeamsConstructProps = {}) {
+    super(scope, id);
+
+    // Define construct contents here
+  }
+}
